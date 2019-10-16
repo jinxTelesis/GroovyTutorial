379979c4cf81badfc9387d60364eb7abe49a9945
Andre Lussier <Dremon7@hotmail.com>
2019-10-15 22:17:21 -0400

    initial commit

---
+++ src/main/java/com/company/LearnGroovyMK1.groovy
@@ -0,0 +1,25 @@
+package com.company
+
+class LearnGroovyMK1 {
+
+    static void main(String... args) {
+        println 'Groovy world!'
+
+
+        def x = 1
+        println x
+
+        x = new java.util.Date()
+        println x
+
+        x = false
+
+        println x
+
+        // empty list
+        def technologies = []
+
+
+
+    }
+}
